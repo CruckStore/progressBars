b19c7a415c8595647a8b08f6951be0ed5bb9cd2b
Cruck Store <cruckstore+github@gmail.com>
2024-08-27 19:09:05 +0200

    Update latest fivem version (fxmanifest) + name change of html file

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+games {'gta5'}
+
+fx_version 'cerulean'
+ui_page 'index.html'
+description 'Progress bar'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+} 
+
+files {
+    'index.html'
+}
+
+export "startUI"
