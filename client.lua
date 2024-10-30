5c466c09169e6fe80a432f72138ea5879a2fd074
Cruck Store <162237671+CruckStore@users.noreply.github.com>
2024-10-30 02:45:09 +0100

    Update client.lua
    Adding Possibility of set % of progression direct on the bar

--- client.lua
+++ client.lua
@@ -1,8 +1,15 @@
-function startUI(time, text) 
-	SendNUIMessage({
-		type = "ui",
-		display = true,
-		time = time,
-		text = text
-	})
+function startUI(time, text)
+    SendNUIMessage({
+        type = "ui",
+        display = true,
+        time = time,
+        text = text
+    })
+end
+
+function setProgress(percentage)
+    SendNUIMessage({
+        type = "progress",
+        percentage = percentage
+    })
 end
